fea4115cd509e1cd5c2d802b30e32b834e51a61a
Chad Kirby <chad.kirby@gmail.com>
2020-10-23 11:59:05 -0700

    feat: implement new jquery methods: add, each, empty, has, map, not
    Also add prev/next sibling(s) methods for handling element contents

--- src/dom.js
+++ src/dom.js
@@ -3,6 +3,7 @@ const globalThis = require('globalthis')();
 let document = globalThis.document;
 let window = globalThis.window;
 let cheerio;
+let jsdom;
 
 module.exports = {
   get document() {
@@ -11,11 +12,15 @@ module.exports = {
   get window() {
     return window;
   },
+  get jsdom() {
+    return jsdom;
+  },
   get cheerio() {
     return cheerio;
   },
   useJSDOM({ JSDOM }) {
-    window = new JSDOM('').window;
+    jsdom = new JSDOM('');
+    window = jsdom.window;
     document = window.document;
   },
   setCheerio(CHEERIO) {
